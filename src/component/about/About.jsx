426b116a59c38d635d57e36ce987fab2ab694168
amit dixit <amitdixit104@gmail.com>
2023-08-22 10:47:40 +0530

    some changes

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FaAccessibleIcon} from 'react-icons/fa'//
+import {VscFolderLibrary} from 'react-icons/vsc'
 const About = () => {
   return (
-   <section id='about'> About</section>
+   <section id='about'>
+    <h5>Get to know</h5>
+    <h2>About me</h2>
+
+ <div className='container about_container'>
+  <div className='about_me'>
+  <div className='about_me.image'>
+    <img src={ME} alt='about image'/>
+  </div>
+  </div>
+  <div className='about_content'>
+<div className='about_cards'>
+  <article className='about_card'>
+    <FaAward className='about_icon'/>
+    <h5>Experience</h5>
+    <small>1+ Year working</small>
+  </article>
+
+  
+   <article className='about_card'>
+   <FaAccessibleIcon className='about_icon'/>
+    <h5>Clients</h5>
+    <small>100+ worldwide</small>
+   </article>
+  
+
+  <article className='about_card'>
+    <VscFolderLibrary className='about_icon'/>
+    <h5>Projects</h5>
+    <small>25+ Completed</small>
+  </article>
+  </div>
+
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis obcaecati fugiat dolorum repellat culpa, qui iste eum asperiores, veniam sint, porro quia fugit velit consectetur saepe commodi voluptates labore similique!</p>
+  <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+  </div>   
+  </div>
+
+      </section>
   )
 }
 
--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -4,16 +4,25 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
-import {AiOutlineMessage} from 'react-icons/ai'
-
+import {BiOutlineMessage} from 'react-icons/ai'
+import { useState } from 'react'
 const Nav = () => {
   return (
    <nav>
-    <a href='#'><AiOutlineHome/></a>
-    <a href='#about'><AiOutlineUser/></a>
-    <a href='#experience'><BiBook/></a>
-    <a href='#services'><RiServiceLine/></a>
-    <a href='#contact'><AiOutlineMessage/></a>
+    <a href='#' onClick={()=> setActiveNav('#')} className=
+    {ActiveNav==='#' ? 'active':''}><AiOutlineHome/></a>
+
+    <a href='#about'onClick={()=> setActiveNav('#about')} className=
+    {ActiveNav==='#' ? 'active':''}><AiOutlineUser/></a>
+
+    <a href='#experience'onClick={()=> setActiveNav('#experience')} className=
+    {ActiveNav==='#experience' ? 'active':''}><BiBook/></a>
+
+    <a href='#services'onClick={()=> setActiveNav('#services')} className=
+    {ActiveNav==='#services' ? 'active':''}><RiServiceLine/></a>
+
+    <a href='#contact'onClick={()=> setActiveNav('#contact')} className=
+    {ActiveNav==='#contact' ? 'active':''}><BiOutlineMessage/></a>
    </nav>
   )
 }
